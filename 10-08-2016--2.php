a2a1194aa7eaee89835ae7e758d21cb4fd3f8973
ferrerguzman <ferrerguzmanmita@gmail.com>
2016-08-10 19:17:26 -0500

    trabajando con date y y un poco de for en php

---
+++ 10-08-2016--2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		echo "<ul>";
+		for ($i=1; $i <=5 ; $i++) 
+		{ 
+			echo "<li><h3>".$i."</li></h3>";
+		}
+		echo "</ul>";
+		/*echo "<h1>emcabezado 1</h1>";
+		echo "<h2>encabezado 2</h2>";
+		echo "<h3>encabezado 3</h3>";
+		echo "<h4>encabezado 4</h4>";
+		echo "<h5>encabezado 5</h5>";
+		echo "<h6>encabezado 6</h6>";*/
+
+	?>
+</body>
+</html>
---
+++ 10-08-2016.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>10-08-2016</title>
+</head>
+<body>
+	<?php 
+		date_default_timezone_set('America/Los_Angeles');
+		echo date("H_i_s");
+	?>
+	<?php
+	echo "Hola"
+	?>
+</body>
+</html>
