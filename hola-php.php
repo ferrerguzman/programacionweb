bfc0771b85e0e79b9e2f32172ae2b1734078e67f
”ferrerguzman” <ferrerguzmanmita@gmail.com>
2016-09-08 07:12:05 -0500

    Agregamos Present + Comp

--- hola-php.php
+++ hola-php.php
@@ -1,10 +1,38 @@
-<html>
+<!doctype html>
+<html lang="es">
 	<head>
-		<title> </title>
+		<title>hola php</title>
+		<style type="text/css">
+		/*presentacion*/
+		h1{color:peru;}
+		p{color:#adcbf7;}
+		li:first-letter{font-size: 2em;}
+		</style>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<?php
-			echo "hola php"
-		¿>
+		<script type="text/javascript">
+		alert("Hola"+ prompt("ingresa tu nombre"));
+		</script>
+		<h1>hola php</h1>
+		<p>Dias de la semana</p>
+		<a class="btn btn-primary" href="http://www.google.com">Ir a google</a>
+		<ol>
+			<li>Lunes</li>
+			<li>Martes</li>
+			<li>Miercoles</li>
+			<li>Jueves</li>
+			<li>Viernes</li>
+			<li>Sabado</li>
+			<li>Domingo</li>
+		</ol>
+
 	</body>
 </html>
